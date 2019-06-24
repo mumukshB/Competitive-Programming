6abc2df969f5ed72d392be3c13edd9ae9c0907cc
Mumuksh Bhargava <30808762+mumukshB@users.noreply.github.com>
2019-06-24 15:26:46 +0530

    Create viral advertising.cpp

---
+++ Hackerrank/viral advertising.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the viralAdvertising function below.
+int viralAdvertising(int n) {
+//int val[51];
+int s=5,l,ans=0;
+for(int i=1;i<=n;i++){
+    l=floor(s/2);
+    ans+=l;
+    s=l*3;
+}
+
+return ans;}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
